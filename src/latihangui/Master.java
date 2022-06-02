7cc986e3674cee12816ba862b99e30fd39b097d8
Kanti Wilujeng <95469164+kantiwilujeng7@users.noreply.github.com>
2022-06-02 15:08:36 +0700

    Add files via upload

---
+++ src/latihangui/Master.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihangui;
+
+/**
+ *
+ * @author Ajeng
+ */
+public interface Master {
+    void tambahData();
+    void ubahData();
+    void hapusData();
+    void tampilData();
+    void bersihForm();
+    
+}
